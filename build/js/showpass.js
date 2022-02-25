1bc23a77734228faf234b81fd534dc78bfc72ff1
mustafa1912 <mustafa.wahba2404@science.menofia.edu.eg>
2022-02-25 14:49:33 +0200

    mostafa

--- build/js/showpass.js
+++ build/js/showpass.js
@@ -1,18 +1,42 @@
 // show & hiden password
-const Password = document.getElementById('Password'),
-    hideshow = document.getElementById('hideshow'),
-    slash = document.getElementById('slash'),
-    eye = document.getElementById('eye');
+const Password = document.querySelectorAll('.Password'),
+    hideshow = document.querySelectorAll('.hideshow'),
+    slash = document.querySelectorAll('.fa-eye-slash'),
+    eye = document.querySelectorAll('.fa-eye');
 // display none to eye
-hideshow.addEventListener('click', () => {
-    slash.classList.toggle('d-none')
-    eye.classList.toggle('d-none')
+
+eye.forEach((ele) => {
+    ele.addEventListener('click', () => {
+
+        slash.forEach((x) => {
+            x.addEventListener('click', () => {
+                Password[0].setAttribute('type', 'password')
+                Password[1].setAttribute('type', 'password')
+            })
+        })
+
+
+    })
 })
-// set type password
-slash.addEventListener('click', () => {
-    Password.setAttribute('type', 'password')
-})
-// set type text
-eye.addEventListener('click', () => {
-    Password.setAttribute('type', 'text')
-})
+
+
+
+
+
+
+
+
+
+
+// hideshow[i].addEventListener('click', () => {
+//         slash[i].classList.toggle('d-none')
+//         eye[i].classList.toggle('d-none')
+//     })
+//     // set type password
+// slash[i].addEventListener('click', () => {
+//         Password[i].setAttribute('type', 'password')
+//     })
+//     // set type text
+// eye[i].addEventListener('click', () => {
+//     Password[i].setAttribute('type', 'text')
+// })
